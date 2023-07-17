cef1c47f1291d956202add6786a6c8edfe0c43ed
biggujo <gen.style22@gmail.com>
2023-07-17 18:49:07 +0300

    Add a PAGE_STEP const

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -4,13 +4,17 @@ const BASE_URL = "https://pixabay.com/api";
 
 const API_KEY = "38308184-41247c978e0d2604524b8abfa";
 
-export async function fetchImagesByQuery(query) {
+export const PAGE_STEP = 40;
+
+export async function fetchImagesByQuery(query, page) {
   const params = {
     key: API_KEY,
     q: query,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
+    page: page,
+    per_page: PAGE_STEP,
   };
 
   const response = await axios.get(`${BASE_URL}`, {
