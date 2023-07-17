59675bd96eb9e4b27d2a22efd0c0bc9389e3d3c0
biggujo <gen.style22@gmail.com>
2023-07-17 19:43:55 +0300

    Rewrite API to class

---
+++ src/js/pixabay-api-service.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+
+const BASE_URL = "https://pixabay.com/api";
+
+axios.defaults.params = {
+  key: "38308184-41247c978e0d2604524b8abfa",
+  image_type: "photo",
+  orientation: "horizontal",
+  safesearch: true,
+};
+
+export class PixabayApiService {
+  static PAGE_STEP = 40;
+
+  #query;
+  #maxPage;
+  #currentPage;
+
+  constructor() {
+    this.query = "";
+    this.currentPage = 1;
+  }
+
+  async fetchImagesByQuery(query) {
+    const params = {
+      q: query,
+      page: this.currentPage,
+      per_page: PixabayApiService.PAGE_STEP,
+    };
+
+    const response = await axios.get(`${BASE_URL}`, {
+      params,
+    });
+
+    console.log(response);
+
+    return response.data;
+  }
+
+  incrementPage() {
+    this.currentPage += 1;
+  }
+
+  resetPage() {
+    this.currentPage = 1;
+  }
+
+  get query() {
+    return this.#query;
+  }
+
+  set query(value) {
+    this.#query = value;
+  }
+
+  get maxPage() {
+    return this.#maxPage;
+  }
+
+  set maxPage(value) {
+    this.#maxPage = value;
+  }
+
+  get currentPage() {
+    return this.#currentPage;
+  }
+
+  set currentPage(value) {
+    this.#currentPage = value;
+  }
+}
