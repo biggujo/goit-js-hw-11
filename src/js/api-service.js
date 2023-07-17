100dac54b9ebc8d0e3b98ac758ab33c9a7dc0081
biggujo <gen.style22@gmail.com>
2023-07-17 17:32:58 +0300

    Rewrite with Axios

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,16 +1,21 @@
+import axios from "axios";
+
 const BASE_URL = "https://pixabay.com/api";
 
 const API_KEY = "38308184-41247c978e0d2604524b8abfa";
 
 export async function fetchImagesByQuery(query) {
-  const parameters = new URLSearchParams({
+  const params = {
     key: API_KEY,
     q: query,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
+  };
+
+  const response = await axios.get(`${BASE_URL}`, {
+    params,
   });
 
-  const response = await fetch(`${BASE_URL}?${parameters}`);
-  return response.json();
+  return response.data;
 }
